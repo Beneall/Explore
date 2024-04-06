0f9c410f5da2ac210abf414b1ae771281b9df7f9
Beneall <benea,63@gmail.com>
2024-04-06 11:45:55 +0300

    update on index and signup php

--- signup.php
+++ signup.php
@@ -45,18 +45,12 @@
     } else {
 ?>
 
- <!--start of signup form -->
- <div class="container">
-  
- <div class="forms-container">
-        <div class="signin-signup">
-          <form action="#" class="sign-in-form">
-          <h2 class="title"></h2>
-            
-          </form>
-
-      <form action="#" class="sign-up-form" method="post">
-            <h2 class="title">Sign up</h2>
+<div class="container sign-up-mode"> <!-- Added class sign-up-mode to initially display sign-up form -->
+    <div class="forms-container">
+      <div class="signin-signup">
+        <!-- Start of sign up form -->
+        <form action="#" class="sign-up-form">
+          <h2 class="title">Sign up</h2>
             <div class="input-field">
               <i class="fas fa-user"></i>
               <input type="text" class="login-input" name="username" placeholder="Username" required />
@@ -70,9 +64,6 @@
               <input type="password" class="login-input" name="password" placeholder="Password">
             </div>
             <input type="submit" name="submit" value="Register" class="btn solid">
-
-
-            
 
 
 
@@ -92,43 +83,74 @@
                 <i class="fab fa-linkedin-in"></i>
               </a>
             </div>
-          </form>
-        </div>
-      </div>
+        </form>
+        <!-- End of sign up form -->
 
-
-
-
-
-
-
-      <div class="panels-container">
-        <div class="panel left-panel">
-          <div class="content">
-            <h3>New here ?</h3>
-            <p>
-              Please sign up to be one of us
-            </p>
-            <button class="btn transparent" id="sign-up-btn">
-              Sign up
-            </button>
+        <!-- Start of sign in form -->
+        <form action="#" class="sign-in-form">
+          <h2 class="title">Sign in</h2>
+          <div class="input-field">
+            <i class="fas fa-user"></i>
+            <input type="text" placeholder="Username" />
           </div>
-          <img src="img/" class="image" alt="" />
-        </div>
-        <div class="panel right-panel">
-          <div class="content">
-            <h3></h3>
-            <p>
-             
-            </p>
-            <button class="btn transparent" id="sign-in-btn">
-              
-            </button>
+          <div class="input-field">
+            <i class="fas fa-lock"></i>
+            <input type="password" placeholder="Password" />
           </div>
-          <img src="signup-login/images/Login.jpg" class="image" alt="" />
-        </div>
+          <input type="submit" value="Login" class="btn solid" />
+
+
+
+
+          <!--Social media links-->
+          <p class="social-text">Or Sign in with social platforms</p>
+          <div class="social-media">
+            <a href="#" class="social-icon">
+              <i class="fab fa-facebook-f"></i>
+            </a>
+            <a href="#" class="social-icon">
+              <i class="fab fa-twitter"></i>
+            </a>
+            <a href="#" class="social-icon">
+              <i class="fab fa-google"></i>
+            </a>
+            <a href="#" class="social-icon">
+              <i class="fab fa-linkedin-in"></i>
+            </a>
+          </div>
+        </form>
+        <!-- End of sign in form -->
       </div>
     </div>
+
+    <div class="panels-container">
+      <div class="panel left-panel">
+        <div class="content">
+          <h3>New here ?</h3>
+          <p>
+            Please sign up to be one of us
+          </p>
+          <button class="btn transparent" id="sign-up-btn">
+            Sign up
+          </button>
+        </div>
+        <img src="img/Sign in.jpg" class="image" alt="" />
+      </div>
+
+      <div class="panel right-panel">
+        <div class="content">
+          <h3>One of us ?</h3>
+          <p>
+            Please login to acess your portal
+          </p>
+          <button class="btn transparent" id="sign-in-btn">
+            Sign in
+          </button>
+        </div>
+        <img src="imgages/Login.jpg" class="image" alt="" />
+      </div>
+    </div>
+  </div>
 
     <script src="signup-login/js/Form.JS"></script>
   
